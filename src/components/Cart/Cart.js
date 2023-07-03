827a30346afc4d42d8dbe5cc1f7ea6e65bac7f65
kamran <ksyedexd@gmail.com>
2023-07-03 22:26:23 +0530

    Changed cart items amount logic and added functionality to increment and decrement cart item count

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,15 +2,30 @@ import React, { useContext } from "react"
 import Modal from "../UI/Modal"
 import classes from "./Cart.module.css"
 import CartContext from "../../Store/cart-context"
+import CartItem from "./CartItem"
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext)
-  const cartTotal = `$${cartCtx.totalAmount}`
+  const cartTotal = `$${cartCtx.totalAmount.toFixed(2)}`
+
+  const addCartItemHandler = (item) => {
+    cartCtx.addItem({...item, amount:1})
+  }
+  const removeCartItemHandler = (id) => {
+    cartCtx.removeItem(id)
+  }
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li key={item.key}>{item.name}</li>
+        <CartItem
+          key={item.key}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onAdd={addCartItemHandler.bind(null,item)}
+          onRemove={removeCartItemHandler.bind(null,item.id)}
+        />
       ))}
     </ul>
   )
